dc855dcd41e99496e6645423462cce5f0e874d43
Dedyanna Cacau <dedyannacacau@gmail.com>
2021-03-29 16:10:24 -0300

    add avatar and  delete avatar

---
+++ src/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,46 @@
+import { NextFunction, Request, Response } from 'express';
+import { verify } from 'jsonwebtoken';
+
+import { AppError } from '../errors/AppError';
+import { UsersRepository } from '../modules/accounts/repositories/implementations/UsersRepository';
+
+interface IPayload {
+  sub: string;
+}
+
+export async function ensureAuthenticated(
+  request: Request,
+  response: Response,
+  next: NextFunction,
+) {
+  const authHeader = request.headers.authorization;
+
+  if (!authHeader) {
+    throw new AppError('Token is missing', 401);
+  }
+
+  const [, token] = authHeader.split(' ');
+
+  try {
+    const { sub: user_id } = verify(
+      token,
+      'c8bf4e1d7b6bd276fa4875f9e6fd085c',
+    ) as IPayload;
+
+    const usersRepository = new UsersRepository();
+
+    const user = usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new AppError('User not found', 401);
+    }
+
+    request.user = {
+      id: user_id,
+    };
+
+    next();
+  } catch {
+    throw new AppError('Token is invalid', 401);
+  }
+}
